740febfa586a4c4c7fb9c2254406ff122575367e
Felix <felix.elias@accenture.com>
2019-10-30 13:49:35 -0400

    added resume file

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = (props) => (
             <div className="inner">
                 <h1>Felix Elias</h1>
                 <p>Online Portfolio</p>
-                <p>Click <a href='#'>here</a> for a download version</p>
+                <p>Click <a href='https://felixelias.com/documents/Felix_Elias_Resume.docx'>here</a> for a download version</p>
             </div>
         </div>
         <nav>
