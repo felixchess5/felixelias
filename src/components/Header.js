99e76ddd3aea88abb241a14dec8e53a52b643929
Felix <felixelias5@gmail.com>
2020-01-12 14:55:04 -0500

    Changed name of portfolio download

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = (props) => (
             <div className="inner">
                 <h1>Felix Elias</h1>
                 <p>Online Portfolio</p>
-                <p>Click <a href='https://felixelias.com/documents/Felix_Elias_Portfolio.docx'>here</a> for a download version</p>
+                <p>Click <a href='https://felixelias.com/documents/Felix_Elias_Portfolio.docx'>here</a> for a download version</p> 
             </div>
         </div>
         <nav>
@@ -32,4 +32,3 @@ Header.propTypes = {
 }
 
 export default Header
-    
